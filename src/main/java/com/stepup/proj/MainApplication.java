6a3cfc320162b05a647c9cb8141cfabb66329ac6
Daniil Kopanev <DKopanev@inno.tech>
2024-11-17 15:03:31 +0300

    Изменена основная логика Добавлен новый метод

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,9 +3,11 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("11111");
+        System.out.println("1001");
     }
     public static void doSomething(){
         System.out.println("123");
+        System.out.println("456");
+        System.out.println("789");
     }
 }
