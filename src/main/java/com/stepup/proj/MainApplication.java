ecbda1ac5ded8643dd89e6ad8691397708894e13
Daniil Kopanev <DKopanev@inno.tech>
2024-11-17 15:17:32 +0300

    Изменили логику main и doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,12 +2,13 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("---");
         System.out.println("Hello World!");
         System.out.println("1001");
     }
     public static void doSomething(){
-        System.out.println("123");
-        System.out.println("456");
-        System.out.println("789");
+        System.out.println(0);
+        System.out.println(1);
+        System.out.println(1);
     }
 }
